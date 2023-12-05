577732a78497dc32b25b3b6f2f9abd1d248ca8a0
mukundmadhav-kalvium <mukund.madhav@kalvium.community>
2023-12-05 17:44:49 +0530

    Part 2 Done

--- app.js
+++ app.js
@@ -9,6 +9,15 @@ class ReactContainer extends React.Component {
   }
 }
 
+// function ReactContainer(){
+//     return (
+//         <div>
+//             Hello! Welcome to Kalvium
+//             <div>Let's rock and roll - this is babel</div>
+//         </div>
+//     )
+// }
+
 const container = document.getElementById("react-container");
 ReactDOM.render(<ReactContainer />, container);
 
